1b3fbf20868dc6f93423536b6c8a2e0e2a556d0d
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-01-17 17:15:52 -1000

    Pokemon array loop

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 let pokemonList = [
-    { name:'Charizard', height:1.7, types:['fire', 'flying']},
     { name:'Pikachu', height:.4, types:'electric'},
     { name:'Squirtle', height:.5, types:'water'},
+    { name:'Charizard', height:1.7, types:['fire', 'flying']},
     { name:'Ninetails', height:1.1, types:'electric'},
 ];
 
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 1.5){
+    document.write(pokemonList[i].name + " Height=" + pokemonList[i].height + "," + " Wow! That's a big Pokemon." + "<p></p>");}
+    else {
+        document.write(pokemonList[i].name + " Height=" + pokemonList[i].height + "<p></p>") ;
+    }
+}
