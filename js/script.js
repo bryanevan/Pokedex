55c86be5e06a23bef2b6848fd395e1fb23db1ed6
bryanevan <35902530+bryanevan@users.noreply.github.com>
2023-01-25 15:57:31 -1000

    1.6 updates

--- js/script.js
+++ js/script.js
@@ -6,33 +6,40 @@ let pokemonRepo = (function () {
     { name:'Charizard', height:1.7, types:['fire', 'flying']},
     { name:'Ninetails', height:1.1, types:'electric'},
   ];
-document.write("<p></p>")
+  document.write("<p></p>");
 
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-function getAll() {
-  return pokemonList;
-}
-function addListItem (pokemon) {
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listItem = document.createElement('li');
-  let button = document.createElement('button');
-  button.innerText = pokemon.name;
-  button.classList.add("button-class");
-  listItem.appendChild(button);
-  pokemonList.appendChild(listItem);
-}
-
-  return {
-  add: add,
-  getAll: getAll,
-  addListItem: addListItem,
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem (pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener("click", function(Event) {
+      showDetails(pokemon);
+    });
+  }
+  
+  function showDetails (pokemon) {
+        console.log(pokemon);
     };
-})();
-    
+  
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+      };
+  })();
 
-pokemonRepo.getAll().forEach(function(pokemon) {
-  pokemonRepo.addListItem(pokemon);
+  pokemonRepo.getAll().forEach(function(pokemon) {
+    pokemonRepo.addListItem(pokemon);
 });
